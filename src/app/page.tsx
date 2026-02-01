ab4bd83c2f2a43aeb9f6f72ae6c29a236d2ec92a
2025848805koushik-cell <2025848805.koushik@ug.sharda.ac.in>
2026-02-01 20:10:47 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Habitual App is Live 🚀</h1>
+      <p>Your habit tracking app deployed successfully.</p>
+    </main>
+  );
+}
