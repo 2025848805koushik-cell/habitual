49a39633318c96fe10d0c094c2aa7105a7d697bb
2025848805koushik-cell <2025848805.koushik@ug.sharda.ac.in>
2026-02-01 20:10:08 +0530

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Habitual App is Live 🚀</h1>
-      <p>Your habit tracking app deployed successfully.</p>
-    </main>
-  );
-}
